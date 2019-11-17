bae18d9c1f676fd99ed408da4f0e41ac55a8ea8f
sudir81 <papinenisudheer@yahoo.com>
2019-11-17 07:41:42 -0500

    Added User model & Schema, also added crypto package to encript password

--- graphql-event-booking/app.js
+++ graphql-event-booking/app.js
@@ -3,8 +3,10 @@ const bodyparser = require("body-parser");
 const httpGraphql = require("express-graphql");
 const { buildSchema } = require("graphql");
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const Event = require("./models/event");
+const User = require("./models/user");
 
 const app = express();
 const events = [];
@@ -24,11 +26,22 @@ app.use(
           date: String!
         }
 
+        type User {
+          _id: ID
+          email: String!
+          password: String
+        }
+
         input EventInput {
           title: String!
           description: String!
           price: Float!
           date: String!
+        }
+
+        input UserInput {
+          email: String!
+          password: String!
         }
 
         type RootQuery {
@@ -37,6 +50,7 @@ app.use(
 
         type RootMutation {
             createEvent(inputEvent: EventInput): Event
+            createUser(inputUser: UserInput): User
         }
         schema {
             query: RootQuery
@@ -61,17 +75,53 @@ app.use(
           title: args.inputEvent.title,
           description: args.inputEvent.description,
           price: +args.inputEvent.price,
-          date: new Date(args.inputEvent.date)
+          date: new Date(args.inputEvent.date),
+          owner: "5dcff67ae9e51db2ea986b36"
         });
 
+        let createdEvent;
         return event
           .save()
           .then(result => {
-            console.log(result);
-            return { ...result._doc, _id: result._doc._id.toString() };
+            createdEvent = { ...result._doc, _id: result._doc._id.toString() };
+            return User.findById("5dcff67ae9e51db2ea986b36");
+          })
+          .then(user => {
+            if (!user) {
+              throw new Error("User not found");
+            }
+
+            user.createdEvents.push(event);
+            return user.save();
+          })
+          .then(result => {
+            return createdEvent;
           })
           .catch(err => {
             console.log(err);
+            throw err;
+          });
+      },
+      createUser: args => {
+        return User.findOne({ email: args.inputUser.email })
+          .then(user => {
+            if (user) {
+              throw new Error("User already exists");
+            }
+            return bcrypt.hash(args.inputUser.password, 12);
+          })
+          .then(encryptedPassword => {
+            const user = new User({
+              email: args.inputUser.email,
+              password: encryptedPassword
+            });
+            return user.save();
+          })
+          .then(result => {
+            console.log(result);
+            return { ...result._doc, _id: result.id };
+          })
+          .catch(err => {
             throw err;
           });
       }
--- graphql-event-booking/models/event.js
+++ graphql-event-booking/models/event.js
@@ -18,6 +18,10 @@ const eventSchema = new Schema({
   date: {
     type: Date,
     required: true
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "User"
   }
 });
 
